2b70ce6122e048c7aa0c058aa4eea2d81f86f177
muskkkannnn <110267124+muskkkannnn@users.noreply.github.com>
2025-12-20 08:19:44 +0530

    Changes #1

--- script.js
+++ script.js
@@ -1,81 +1,51 @@
-<<<<<<< HEAD
-=======
-// <!-- Locomotive CDN  -->
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('.main'),
-    smooth: true
-});
+function loader() {
+    var tl = gsap.timeline()
 
-// Loading animation
->>>>>>> e24189654ef97c924d972d8eb14909054b9814b8
-var tl = gsap.timeline()
+    tl.to(".yellow1", {
+        top: "-100%",
+        duration: 0.5,
+        delay: 0.4,
+        ease: "expo.in",
+    })
+    tl.to(".headline", {
+        color: "#000",
+        duration: 0.3,
+    }, "<")
 
-tl.to(".yellow1", {
-    top: "-100%",
-    duration: 0.5,
-    delay: 0.4,
-    ease: "expo.in",
-})
-<<<<<<< HEAD
-tl.to(".headline", {
-    color: "#000",
-    duration: 0.3,
-}, "<")
-=======
-.to(".headline", {
-  color: "black"
-}, "<");
->>>>>>> e24189654ef97c924d972d8eb14909054b9814b8
+    tl.to(".bg-video", {
+        duration: 0.5,
+        delay: -0.2,
+    })
+    tl.to(".headline", {
+        color: "#fff",
+        duration: 0.3,
+    }, "<")
 
-tl.to(".bg-video", {
-    duration: 0.5,
-    delay: -0.2,
-})
-<<<<<<< HEAD
-tl.to(".headline", {
-    color: "#fff",
-    duration: 0.3,
-}, "<")
-=======
-.to(".headline", {
-  color: "white"
-}, "<");
->>>>>>> e24189654ef97c924d972d8eb14909054b9814b8
+    tl.from(".yellow2", {
+        top: "100%",
+        duration: 0.5,
+        delay: 0.8,
+        ease: "expo.out",
+    })
+    tl.to(".headline", {
+        color: "#000",
+        duration: 0.3,
+    }, "<")
 
-tl.from(".yellow2", {
-    top: "100%",
-    duration: 0.5,
-    delay: 0.8,
-    ease: "expo.out",
-})
-<<<<<<< HEAD
-tl.to(".headline", {
-    color: "#000",
-    duration: 0.3,
-}, "<")
+    tl.to(".loader", {
+        opacity: 0,
+    })
 
-tl.to(".loader",{
-    opacity: 0,
-})
-=======
-.to(".headline", {
-  color: "black"
-}, "<");
->>>>>>> e24189654ef97c924d972d8eb14909054b9814b8
+    tl.to(".loader", {
+        display: "none",
+    })
 
-tl.to(".loader",{
-    display: "none",
-})
-<<<<<<< HEAD
- 
-tl.to(".headline", {
-    top: "-20%",
-    duration: 0.6,
-    delay: 0.09,
-    ease: "ease.inOut(4)",
-})
-=======
-tl.to(".loader",{
-    opacity: 0,
-})
->>>>>>> e24189654ef97c924d972d8eb14909054b9814b8
+    tl.to(".headline", {
+        top: "-20%",
+        duration: 0.6,
+        delay: 0.09,
+        ease: "ease.inOut(4)",
+    })
+}
+
+loader();
